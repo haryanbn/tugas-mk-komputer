987e1d62f53f46922dcf75cfad86f625373b2195
haryanbn <148609364+haryanbn@users.noreply.github.com>
2023-11-02 22:12:59 +0700

    Add files via upload

---
+++ haryan_2.m
@@ -0,0 +1,27 @@
+
+% NAMA : HARYAN SAPRIL 
+% NIM : 112020139
+% KELAS : D
+
+clc 
+clear
+
+% SOAL 4.2 
+% dengan menggunakan Mathlab, buatlah
+
+% a. matrik indentitas dengan dimensi 5x5
+a = eye (5,5)
+
+% b. matrik dengan dimensi 3x3 dengan elemen semuanya bernilai nol
+b = zeros (3,3)
+
+% c. matrik dengan dimensi 4x4 dengan elemen semuanya bernilai satu
+c = ones (4,4)
+
+%d. matrik dengan dimensi 3x4 dimana elemen terdistribusi anatara 1 dan 0 
+% (default n=m)
+d = rand (3,4) 
+
+% e. matrik dengan dimensi 3x4 dimana elemen terdistribusi normal 
+e = randn (3,4)
+
